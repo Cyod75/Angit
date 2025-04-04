588f349f81c7f2de43a082f6590e07e13c61f794
Angie Yague <angietoledanovillalba@gmail.com>
2025-04-04 21:47:34 +0100

    Version Inicial

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Obtiene el botón
+const moreInfoBtn = document.getElementById('moreInfoBtn');
+
+// Evento para el botón
+moreInfoBtn.addEventListener('click', function() {
+    alert("Más información sobre Juan Pérez: \nJuan es un desarrollador web con experiencia en HTML, CSS, JavaScript, y frameworks modernos como React.");
+});
